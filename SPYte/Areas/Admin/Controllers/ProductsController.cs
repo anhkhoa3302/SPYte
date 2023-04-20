0cacc94135bd4b524d7738aaebaa4d70c2736718
Vu Nguyen <vuky12@icloud.com>
2023-04-21 03:44:19 +0700

    Update Status product, fix layout product in admin website
    Trong model Category, hình như thiếu trường status để khi xoá cũng cập nhật status như Product,
    Còn cho nên chức năng xoá Category là k thực hiện được, còn quản lý user, t thử add model ApplicationUser vào controller lúc tạo mà k được.

--- SPYte/Areas/Admin/Controllers/ProductsController.cs
+++ SPYte/Areas/Admin/Controllers/ProductsController.cs
@@ -2,9 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
+using SPYte.Areas.Identity.Pages.Account.Manage;
 using SPYte.Data;
 using SPYte.Models;
 
@@ -13,19 +16,25 @@ namespace SPYte.Areas.Admin.Controllers
     [Area("Admin")]
     public class ProductsController : Controller
     {
+        private readonly UserManager<ApplicationUser> _userManager;
         private readonly ApplicationDbContext _context;
 
-        public ProductsController(ApplicationDbContext context)
+        public ProductsController(ApplicationDbContext context, UserManager<ApplicationUser> userManager)
         {
             _context = context;
+            _userManager = userManager;
         }
 
-        // GET: Admin/Products
+        //GET: Admin/Products
         public async Task<IActionResult> Index()
         {
             var applicationDbContext = _context.Products.Include(p => p.User);
             return View(await applicationDbContext.ToListAsync());
         }
+
+
+
+
 
         // GET: Admin/Products/Details/5
         public async Task<IActionResult> Details(long? id)
@@ -90,15 +99,53 @@ namespace SPYte.Areas.Admin.Controllers
         // POST: Admin/Products/Edit/5
         // To protect from overposting attacks, enable the specific properties you want to bind to.
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public async Task<IActionResult> Edit(long id, [Bind("Id,Name,Summary,Description,Price,Stock,Unit,IsVisible,Status,CreatedDate,UpdatedDate,UserId")] Product product)
+        //{
+        //    if (id != product.Id)
+        //    {
+        //        return NotFound();
+        //    }
+
+        //    if (ModelState.IsValid)
+        //    {
+        //        try
+        //        {
+
+        //            _context.Update(product);
+        //            await _context.SaveChangesAsync();
+        //        }
+        //        catch (DbUpdateConcurrencyException)
+        //        {
+        //            if (!ProductExists(product.Id))
+        //            {
+        //                return NotFound();
+        //            }
+        //            else
+        //            {
+        //                throw;
+        //            }
+        //        }
+        //        return RedirectToAction(nameof(Index));
+        //    }
+        //    ViewData["UserId"] = new SelectList(_context.Users, "Id", "Id", product.UserId);
+        //    return View(product);
+        //}
+
+
+        //POST: Products/Edit/5
+         //To protect from overposting attacks, enable the specific properties you want to bind to, for 
+         //more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(long id, [Bind("Id,Name,Summary,Description,Price,Stock,Unit,IsVisible,Status,CreatedDate,UpdatedDate,UserId")] Product product)
+        //[Authorize(Roles = "Admin")]
+        public async Task<IActionResult> Edit(long id, [Bind("Id,UserId,Title,Summary,Content,Price,Quantity,Unit,Status,CreatedAt,UpdatedAt")] Product product)
         {
             if (id != product.Id)
             {
                 return NotFound();
             }
-
             if (ModelState.IsValid)
             {
                 try
@@ -117,10 +164,12 @@ namespace SPYte.Areas.Admin.Controllers
                         throw;
                     }
                 }
-                return RedirectToAction(nameof(Index));
+                return Redirect("/Identity/Account/Manage/UserProducts");
             }
-            ViewData["UserId"] = new SelectList(_context.Users, "Id", "Id", product.UserId);
-            return View(product);
+            else
+            {
+                return NotFound("Model state invalid");
+            }
         }
 
         // GET: Admin/Products/Delete/5
@@ -147,16 +196,23 @@ namespace SPYte.Areas.Admin.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> DeleteConfirmed(long id)
         {
-            if (_context.Products == null)
-            {
-                return Problem("Entity set 'ApplicationDbContext.Products'  is null.");
-            }
             var product = await _context.Products.FindAsync(id);
-            if (product != null)
+            if (product == null)
             {
-                _context.Products.Remove(product);
+                return NotFound();
             }
-            
+            product.IsVisible = 2; // đặt isVisible là 2, để ký hiệu là không còn hữu dụng
+            product.Status = 2;
+            _context.Entry(product).State = EntityState.Modified;
+
+            // Cập nhật bản ghi của các bảng khác trước khi xoá sản phẩm
+            var productCategories = await _context.ProductCategory.Where(pc => pc.ProductId == id).ToListAsync();
+            foreach (var pc in productCategories)
+            {
+                _context.ProductCategory.Update(pc);
+            }
+
+            // Lưu lại thay đổi và chuyển hướng trang
             await _context.SaveChangesAsync();
             return RedirectToAction(nameof(Index));
         }
